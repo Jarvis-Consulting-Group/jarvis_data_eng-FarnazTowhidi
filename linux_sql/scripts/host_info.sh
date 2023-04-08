a616b6ca3954d0d4144c4eb4032be2086a6f6e88
Farnaz Towhidi <farnaz.towhidi@gmail.com>
2023-04-08 00:18:38 +0000

    init host_info script

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,28 @@
+#Setup and validate arguments (again, don't copy comments)
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+#Check # of args
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+#Save machine statistics in MB and current machine hostname to variables
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+#Retrieve hardware specification variables
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb" | tail -1 | awk '{print $15}')
+cpu_kernel=$(echo "$vmstat_mb" | tail -1 | awk '{print $14}')
+
+
+#Current time in `2019-11-26 14:40:19` UTC format
+timestamp=$(vmstat -t | tail -1 | awk '{print $18,$19}')
+
+
+echo $timestamp
