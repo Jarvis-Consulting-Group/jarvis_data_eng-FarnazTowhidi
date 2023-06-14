401ac8e35ff342fa30dd8037e931a4b31f83dffc
Farnaz Towhidi <farnaz.towhidi@gmail.com>
2023-06-14 13:36:34 +0000

    Solve mockito body response error

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoUnitTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoUnitTest.java
@@ -2,6 +2,7 @@ package ca.jrvs.apps.twitter.dao;
 
 import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
 import ca.jrvs.apps.twitter.model.Tweet;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.URI;
@@ -12,6 +13,9 @@ import org.apache.http.entity.StringEntity;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import static org.mockito.Mockito.*;
 
 
@@ -38,17 +42,14 @@ public class TwitterDaoUnitTest {
     Tweet tweet = new Tweet();
     tweet.setText("Today is a test day 400");
     String tweetJason = "{\"text\": \"" + tweet.getText() + "\"}";
+    StringEntity stringEntity = new StringEntity(tweetJason);
 
 
-    when(mockInputStream.read()).thenReturn(1);
+    when(mockHttpEntity.getContent()).thenReturn(stringEntity.getContent());
+    when(mockHttpResponse.getEntity()).thenReturn(mockHttpEntity);
     when(mockStatusLine.getStatusCode()).thenReturn(201);
-    when(mockStringEntity.getContent()).thenReturn(mockInputStream);
-
-    when(mockHttpResponse.getEntity()).thenReturn(mockStringEntity); //Content=null
-    //when(mockHttpResponse.getEntity()).thenReturn(mockStringEntity); //Content=null
     when(mockHttpResponse.getStatusLine()).thenReturn(mockStatusLine);
-    //when(mockTwitterHttpHelper.httpPost(URI.create("https://api.twitter.com/2/tweets"),tweet.getText())).thenReturn(mockHttpResponse);
-
+    when(mockTwitterHttpHelper.httpPost(URI.create("https://api.twitter.com/2/tweets"),tweet.getText())).thenReturn(mockHttpResponse);
     twitterDao.create(tweet);
   }
 
