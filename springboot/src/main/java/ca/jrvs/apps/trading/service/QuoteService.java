55caad9b951eea4ceaa783a44cee1cd0fcc5b817
farnaz <farnaz.towhidi@gmail.com>
2023-09-14 09:15:06 -0400

    update QuoteService for Show IexQuote

--- springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
@@ -2,26 +2,31 @@ package ca.jrvs.apps.trading.service;
 
 import ca.jrvs.apps.trading.dao.MarketDataDao;
 import ca.jrvs.apps.trading.dao.QuoteDao;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import ca.jrvs.apps.trading.model.domain.IexQuote;
 
-import java.util.logging.Logger;
+import java.util.Optional;
 
 public class QuoteService {
     // calls MarketDataDao methods which queries Quote data from IEX REST API (in JSON format)
     /**
      *
      */
-//    @Transactional
-//    @Service
-    //private static final Logger logger = LoggerFactory.getLogger(QuoteService.class);
 
     private QuoteDao quoteDao;
     private MarketDataDao marketDataDao;
 
     public QuoteService(QuoteDao quoteDao, MarketDataDao marketDataDao) {
+
         this.quoteDao = quoteDao;
         this.marketDataDao = marketDataDao;
+
+    }
+
+    public IexQuote findIexQuoteByTicker(String ticker){
+        Optional<IexQuote> iexQuote = marketDataDao.findById(ticker);
+
+        if(!iexQuote.isPresent()) throw new IllegalArgumentException("ticker is invalid");
+
+        return iexQuote.get();
     }
 }
